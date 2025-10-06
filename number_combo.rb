3fec122126c27e90bdd962aac09cc8d84282036f
Vignesh B <vignesh@mallow-tech.com>
2025-10-06 10:34:47 +0530

    Week 5 - number combo

---
+++ number_combo.rb
@@ -0,0 +1,16 @@
+digits = gets.chomp
+keypad = {
+    "2" => ["a", "b", "c"],
+    "3" => ["d", "e", "f"],
+    "4" => ["g", "h", "i"],
+    "5" => ["j", "k", "l"],
+    "6" => ["m", "n", "o"],
+    "7" => ["p", "q", "r", "s"],
+    "8" => ["t", "u", "v"],
+    "9" => ["w", "x", "y", "z"]
+  }
+
+letters = digits.chars.map { |d| keypad[d] }
+return [] if letters.empty?
+
+p letters.shift.product(*letters).map(&:join)
